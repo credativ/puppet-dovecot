4ae5858bdacc572ae8fa7f0659fc1d04de2ad390
Bastian Blank <bastian.blank@credativ.de>
2014-06-24 14:12:36 +0200

    Add tests for other protocols

--- spec/defines/protocol_spec.rb
+++ spec/defines/protocol_spec.rb
@@ -2,12 +2,31 @@ require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 describe 'dovecot::protocol' do
-  context 'Default imap' do
+  context 'imap' do
     let(:title) { 'imap' }
 
     it {
       should contain_class('dovecot')
       should contain_package('dovecot-imapd')
+    }
+  end
+
+  context 'sieve' do
+    let(:title) { 'sieve' }
+
+    it {
+      should contain_class('dovecot')
+      should contain_package('dovecot-managesieved')
+    }
+  end
+
+  context 'unknown' do
+    let(:title) { 'unknown' }
+
+    it {
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /selector param/)
     }
   end
 end
